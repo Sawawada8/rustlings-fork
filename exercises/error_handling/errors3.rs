4d3bc26d3300d9d13778d787f601bc5874720064
Sawawada8 <sawadadamskz7575@gmail.com>
2023-01-13 21:57:42 +0900

    done errors

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -4,11 +4,12 @@
 // Why not? What should we do to fix it?
 // Execute `rustlings hint errors3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// // I AM NOT DONE
 
 use std::num::ParseIntError;
 
-fn main() {
+// error のときのみ値を返す感じ
+fn main()  -> Result<(), ParseIntError> {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
@@ -20,6 +21,7 @@ fn main() {
         tokens -= cost;
         println!("You now have {} tokens.", tokens);
     }
+    Ok(())
 }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
